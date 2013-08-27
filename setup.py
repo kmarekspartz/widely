431b3b83f7d9bc38c827bb85dbd10c0e4ea559ef
Kyle Marek-Spartz <zeckalpha@gmail.com>
2013-08-27 10:56:37 -0500

    More functionality

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='widely',
+    version='0.1',
+    description='Heroku-style Static-site Platform as a Service command line tool',
+    long_description=open('README.md').read(),
+    author='Kyle Marek-Spartz and Michael Burling',
+    author_email='kyle.marek.spartz@gmail.com',
+    py_modules=['widely'],
+    url='...',
+    include_package_data=True,
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['boto', 'docopt', 'prettytable', 'feedparser'],
+    entry_points={
+        'console_scripts': [
+            'widely = widely:main',
+        ],
+    },
+    test_suite='nose.collector',
+    classifiers=["Private :: Do Not Upload"],  ## To be added later.
+    license='ISC'  ## To be added later.
+)
--- widely.py
+++ widely.py
@@ -3,20 +3,20 @@
 Widely.
 
 Usage:
+  widely [login | auth:login]
+  widely [logout | auth:logout]
+  widely auth:whoami
   widely sites
   widely sites:info [--site <SITENAME>]
   widely sites:create [<SITENAME>]
   widely sites:rename <NEWNAME> [--site <SITENAME>]
-  widely [login | auth:login]
-  widely [logout | auth:logout]
-  widely auth:whoami
-  widely domains
+  widely domains [--site <SITENAME>]
+  widely open
+  widely local [-p <PORT>|--port <PORT>]
+  widely status
   widely logs
   widely (help|-h|--help) [<TOPIC>]
-  widely status
   widely (version|--version)
-  widely local [-p <PORT>|--port <PORT>]
-  widely open
 
 Options:
   -p --port
@@ -30,9 +30,25 @@ import sys
 __version__ = 'widely/0.1 python/' + sys.version
 
 
-def _help(arguments, topic):
-    if topic:  # and topic in help_messages:
-        print('help about ' + topic)  # help_messages[topic]
+def sizeof_fmt(num):
+    """
+    Formats number of bytes into a human readable form.
+
+    From: http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+    """
+    for x in ['bytes','KB','MB','GB']:
+        if -1024.0 < num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+    return "%3.1f %s" % (num, 'TB')
+
+
+def _help(arguments):
+    topic = arguments['<TOPIC>']
+    help_messages = {}
+    if topic and topic in help_messages:
+        print('help about ' + topic)
+        print(help_messages[topic])
     else:
         print(__doc__.strip("\n"))
 
@@ -45,15 +61,13 @@ def get_credentials(arguments):
     """
     Returns current access_key and secret_access_key or calls auth_login()
     """
-    import os
     try:
-        # read boto for access_key
         import boto
         aws_access_key_id = boto.config.get('Credentials', 'aws_access_key_id')
         aws_secret_access_key = boto.config.get('Credentials', 'aws_secret_access_key')
         assert aws_access_key_id is not None
         assert aws_secret_access_key is not None
-    except:
+    except AssertionError:
         print('No credentials found.')
         return auth_login(arguments)
     return aws_access_key_id, aws_secret_access_key
@@ -70,7 +84,7 @@ def auth_login(arguments):
     except DuplicateSectionError:
         pass
 
-    ## Chack for valid credentials ?
+    ## Check for valid credentials ?
 
     boto.config.set('Credentials',
                     'aws_access_key_id',
@@ -104,69 +118,173 @@ def auth_whoami(arguments):
     print(aws_access_key_id)
 
 
+def get_buckets():
+    ## if not logged in, login
+
+    from boto.s3.connection import S3Connection
+    conn = S3Connection()
+    buckets = conn.get_all_buckets()
+    return buckets
+
+
+class NoSuchBucket(Exception):
+    pass
+
+
+class NoWidelyDotfile(Exception):
+    pass
+
+
+def get_specified_bucket(sitename):
+    from boto.s3.connection import S3Connection
+    conn = S3Connection()
+    from boto.exception import S3ResponseError
+    try:
+        bucket = conn.get_bucket(sitename)
+        bucket.get_website_configuration()
+    except S3ResponseError:
+        raise NoSuchBucket
+    return bucket
+
+
+def get_current_bucket():
+    try:
+        sitename = open('.widely', 'r').read()
+    except IOError:
+        raise NoWidelyDotfile
+    return get_specified_bucket(sitename)
+
+
+def get_current_or_specified_bucket(arguments):
+    try:
+        sitename = arguments['<SITENAME>']
+        if sitename:
+            return get_specified_bucket(sitename)
+        else:
+            return get_current_bucket()
+    except NoSuchBucket:
+        print(' !\tSite not found')
+        sys.exit()
+    except NoWidelyDotfile:
+        print(' !\tNo site specified.')
+        print(' !\tRun this command from a site folder or specify which site to use with --site SITENAME.')
+        sys.exit()
+
+
+def websites_from_buckets(buckets):
+    from boto.exception import S3ResponseError
+    for bucket in buckets:
+        try:
+            bucket.get_website_configuration()
+            yield bucket
+        except S3ResponseError:
+            continue
+
+
+def bucket_size(bucket):
+    """
+    Return the number of bytes in a bucket.
+    """
+    return sum(map(lambda x: x.size, bucket.get_all_keys()))
+
+
 def sites(arguments):
     """
-    === My Apps
-    fresnel
-    lexitecture-hubot
+    Displays sites, which are simply the set of buckets which are web sites.
     """
-    pass
+    buckets = get_buckets()
+    websites = websites_from_buckets(buckets)
+    print('=== My sites')
+    for site in websites:
+        print(site.name)
 
 
-def sites_info(arguments, sitename=None):
+def sites_info(arguments):
     """
-    === fresnel
-    Git URL:       git@heroku.com:fresnel.git
-    Owner Email:   zeckalpha@gmail.com
-    Region:        us
-    Repo Size:     108M
-    Slug Size:     24M
-    Stack:         cedar
-    Tier:          Legacy
-    Web URL:       http://fresnel.herokuapp.com/
+    Displays detailed information about the current or specified site.
     """
-    pass
+    bucket = get_current_or_specified_bucket(arguments)
+    print('=== {0}'.format(bucket.name))
+    print('Owner:   {0}'.format(bucket.get_acl().owner.display_name))
+    print('Size:    {0}'.format(sizeof_fmt(bucket_size(bucket))))
+    print('Web URL: {0}'.format(bucket.get_website_endpoint()))
 
 
-def sites_create(arguments, sitename=None):
-    pass
+def sites_create(arguments):
+    """
+    Creates a new site for the specified or current site, or a randomly assigned site name.
+    """
+    f = open('.widely', 'w')
 
 
-def sites_rename(arguments, sitename=None):
+def sites_rename(arguments):
+    """
+    Renames the specified or current site to the new name.
+    """
+    # Create a new bucket with the new name
+    # Copy the configuration over
+    # Change the .widely
+    # Verify the deletion of the old sitename
     pass
 
 
 def domains(arguments):
     """
-    === fresnel Domain Names
-    fresnel.herokuapp.com
+    Lists domains for the specified or current site.
     """
-    pass
+    bucket = get_current_or_specified_bucket(arguments)
+    print('=== {0} Domain Names'.format(bucket.name))
+    print(bucket.get_website_endpoint())
 
 
 def logs(arguments):
-    """(Logs for that site)
-    Not found
     """
+    Shows access and status logs for the specified or current site.
+    """
+    # (Logs for sitename)
+    # Not found
     pass
 
 
 def status(arguments):
     """
-    === Heroku Status
-    Development: No known issues at this time.
-    Production:  No known issues at this time.
+    Shows the S3 system status.
     """
-    pass
+    import feedparser
+    from prettytable import PrettyTable
+    s3_status_rss_feeds = {
+        "N. California": "http://status.aws.amazon.com/rss/s3-us-west-1.rss",
+        "Oregon": "http://status.aws.amazon.com/rss/s3-us-west-2.rss",
+        "US Standard": "http://status.aws.amazon.com/rss/s3-us-standard.rss",
+        "Sao Paulo": "http://status.aws.amazon.com/rss/s3-sa-east-1.rss",
+        "Ireland": "http://status.aws.amazon.com/rss/s3-eu-west-1.rss",
+        "Singapore": "http://status.aws.amazon.com/rss/s3-ap-southeast-1.rss",
+        "Sydney": "http://status.aws.amazon.com/rss/s3-ap-southeast-2.rss",
+        "Tokyo": "http://status.aws.amazon.com/rss/s3-ap-northeast-1.rss"
+    }
+    print('=== S3 Status')
+    table = PrettyTable(['Region', 'Status'])
+    for region_name, rss_url in s3_status_rss_feeds.iteritems():
+        d = feedparser.parse(rss_url)
+        if d.entries:
+            status = d.entries[0].title
+        else:
+            status = 'Unknown.'
+        table.add_row([region_name, status])
+    print(table)
 
 
-def local(arguments, port=None):
+def local(arguments):
+    """
+    Runs the site in a local server, on the specified port if there is one.
+    """
+    port = arguments['<PORT>']
     if port:
         try:
             port = int(port)
         except ValueError:
             print('<PORT> must be an integer.')
-            sys.exitarguments
+            sys.exit()
     else:
         port = 8000
     import SimpleHTTPServer
@@ -175,19 +293,27 @@ def local(arguments, port=None):
     Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     httpd = SocketServer.TCPServer(("", port), Handler)
     print("serving at 0.0.0.0:" + str(port))
-    # _openarguments  # ???
-    httpd.serve_foreverarguments
+    import webbrowser
+    webbrowser.open_new_tab('http://0.0.0.0:' + str(port))
+    httpd.serve_forever()
+
+
 
 
 def _open(arguments):
+    """
+    Loads the running specified or current site in the webbrowser.
+    """
     import webbrowser
-    webbrowser.open_new_tab('http://0.0.0.0:8000')  # __url__ ?
+    url = 'someurl'
+    webbrowser.open_new_tab(url)
 
 
-def main(arguments):
+def main():
     """
     Dispatch based on arguments.
     """
+    arguments = docopt(__doc__, version=__version__, help=False)
     if arguments['version']:
         version(arguments)
     elif arguments['--help'] or arguments['help']:
@@ -217,10 +343,10 @@ def main(arguments):
     elif arguments['status']:
         status(arguments)
     else:
+        print('We did not recognize your specified arguments.')
         print(arguments)
     sys.exit()
 
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version=__version__, help=False)
-    main(arguments)
+    main()
