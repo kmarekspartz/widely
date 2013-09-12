77aeb6060d57544c3844f1803783597c12b82a2b
Kyle Marek-Spartz <zeckalpha@gmail.com>
2013-09-12 11:33:59 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='widely',
     version='0.1',
-    description='Heroku-style Static-site Platform as a Service command line tool',
+    description='Static Site as a Service using AWS S3',
     long_description=open('README.md').read(),
     author='Kyle Marek-Spartz and Michael Burling',
     author_email='kyle.marek.spartz@gmail.com',
     py_modules=['widely'],
-    url='...',
+    url='http://www.celador.mn/widely',
     include_package_data=True,
     packages=find_packages(exclude=['tests*']),
     install_requires=['boto', 'docopt', 'prettytable', 'feedparser'],
@@ -18,6 +18,18 @@ setup(
         ],
     },
     test_suite='nose.collector',
-    classifiers=["Private :: Do Not Upload"],  ## To be added later.
+    classifiers=["Private :: Do Not Upload",  ## To be removed later.
+                 "Development Status :: 3 - Alpha",
+                 "Environment :: Console",
+                 "Intended Audience :: Developers",
+                 "Intended Audience :: Information Technology",
+                 "Intended Audience :: System Administrators",
+                 "License :: OSI Approved :: MIT License",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 2 :: Only",
+                 "Topic :: Internet",
+                 "Topic :: Internet :: WWW/HTTP",
+                 "Topic :: Internet :: WWW/HTTP :: Site Management",
+                 "Topic :: Utilities"],
     license='MIT'
 )
